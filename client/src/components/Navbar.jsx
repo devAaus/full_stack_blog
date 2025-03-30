61796562a01748486b77a06f98ab4a10a51ebf3f
Aayush <aausgh@gmail.com>
2025-03-30 14:11:43 +0545

    clerk integrated for authentication

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react"
 import { IoMenu } from "react-icons/io5";
-import { IKImage } from 'imagekitio-react';
 import Image from "./Image";
+import { Link } from "react-router";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 const Navbar = () => {
    const [open, setopen] = useState(false);
 
    return (
       <div className="w-full h-16 md:h-20 flex items-center justify-between">
-         <div className="flex items-center gap-4 text-2xl font-bold">
+         <Link to={"/"} className="flex items-center gap-4 text-2xl font-bold">
             <Image
                src="/logo.png"
                alt="logo"
                w={100}
                h={100}
             />
-         </div>
+         </Link>
 
          {/* Mobile Menu */}
          <div className="md:hidden">
@@ -40,13 +41,18 @@ export default Navbar
 const AllLinks = () => {
    return (
       <>
-         <a href="/">Home</a>
-         <a href="/">Trending</a>
-         <a href="/">Most Popular</a>
-         <a href="/">About</a>
-         <a href="/">
-            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login</button>
-         </a>
+         <Link to="/">Home</Link>
+         <Link to="/">Trending</Link>
+         <Link to="/">Most Popular</Link>
+         <Link to="/">About</Link>
+         <SignedOut>
+            <Link to="/login">
+               <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login</button>
+            </Link>
+         </SignedOut>
+         <SignedIn>
+            <UserButton />
+         </SignedIn>
       </>
    )
 }
