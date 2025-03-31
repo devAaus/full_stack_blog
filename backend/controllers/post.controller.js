834e61691c3b09b4bc08db583073901ac2f03893
Aayush <aausgh@gmail.com>
2025-03-31 14:15:54 +0545

    integrated clerk

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import Post from '../models/post.model.js';
+import User from '../models/user.model.js';
 
 export const getPosts = async (req, res) => {
    const posts = await Post.find()
@@ -15,7 +16,23 @@ export const getPostbySlug = async (req, res) => {
 }
 
 export const createPost = async (req, res) => {
-   const newPost = new Post(req.body);
+   const clertUserId = req.auth.userId;
+
+   if (!clertUserId) {
+      return res.status(401).json({ message: 'Not authenticated' });
+   }
+
+   const user = await User.findOne({ clertUserId })
+
+   if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+   }
+
+   const newPost = new Post({
+      user: user._id,
+      ...req.body
+   });
+
    const post = await newPost.save();
    if (!post) {
       return res.status(400).json({ message: 'Post not created' });
@@ -25,9 +42,22 @@ export const createPost = async (req, res) => {
 }
 
 export const deletePost = async (req, res) => {
-   const post = await Post.findByIdAndDelete(req.params.id);
+
+   const clertUserId = req.auth.userId;
+
+   if (!clertUserId) {
+      return res.status(401).json({ message: 'Not authenticated' });
+   }
+
+   const user = await User.findOne({ clertUserId })
+
+   const post = await Post.findByIdAndDelete({
+      _id: req.params.id,
+      user: user._id
+   });
+
    if (!post) {
-      return res.status(404).json({ message: 'Post not found' });
+      return res.status(403).json({ message: 'Not authorized to delete this post' });
    }
 
    res.status(200).json({ message: 'Post deleted' });
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import 'dotenv/config'
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 
+import { connectDB } from './lib/connectDb.js';
 import userRouter from './routes/user.route.js';
 import postRouter from './routes/post.route.js';
 import commentRouter from './routes/comment.route.js';
-import { connectDB } from './lib/connectDb.js';
+import webHookRouter from './routes/webhook.route.js';
+
 
 const app = express();
-app.use(express.json());
+app.use(clerkMiddleware())
 
+app.use("/webhooks", webHookRouter)
+
+app.use(express.json());
 
 // routes
 app.use("/users", userRouter)
