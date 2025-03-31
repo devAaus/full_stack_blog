ce1df2e270185be2e82af4984be65149d51d4550
Aayush <aausgh@gmail.com>
2025-03-31 17:31:00 +0545

    create post controller updated

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,11 +1,13 @@
 import Post from '../models/post.model.js';
 import User from '../models/user.model.js';
 
+// get all posts
 export const getPosts = async (req, res) => {
    const posts = await Post.find()
    res.status(200).json(posts);
 }
 
+// get a post
 export const getPostbySlug = async (req, res) => {
    const post = await Post.findOne({ slug: req.params.slug })
    if (!post) {
@@ -15,32 +17,47 @@ export const getPostbySlug = async (req, res) => {
    res.status(200).json(post);
 }
 
+// create post
 export const createPost = async (req, res) => {
-   const clertUserId = req.auth.userId;
+   console.log(req);
 
-   if (!clertUserId) {
+   const clerkUserId = req.auth.userId;
+
+   if (!clerkUserId) {
       return res.status(401).json({ message: 'Not authenticated' });
    }
 
-   const user = await User.findOne({ clertUserId })
+   const user = await User.findOne({ clerkUserId })
 
    if (!user) {
       return res.status(404).json({ message: 'User not found' });
    }
 
-   const newPost = new Post({
-      user: user._id,
-      ...req.body
-   });
+   let slug = req.body.title.trim().replace(/\s+/g, '-').toLowerCase();
+   let existingPost = await Post.findOne({ slug });
 
-   const post = await newPost.save();
-   if (!post) {
-      return res.status(400).json({ message: 'Post not created' });
+   if (existingPost) {
+      let counter = 1;
+      let newSlug = `${slug}-${counter}`;
+
+      // Keep incrementing counter until a unique slug is found
+      while (await Post.findOne({ slug: newSlug })) {
+         counter++;
+         newSlug = `${slug}-${counter}`;
+      }
+      slug = newSlug;
    }
 
+
+   const post = await new Post({
+      user: user._id,
+      slug,
+      ...req.body,
+   }).save();
    res.status(201).json(post);
 }
 
+// delete post
 export const deletePost = async (req, res) => {
 
    const clertUserId = req.auth.userId;
