4b344c8287c2e749280fcf1096e09e4e680f6f66
Aayush <aausgh@gmail.com>
2025-03-31 12:47:55 +0545

    models created

---
+++ backend/models/comment.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from 'mongoose';
+
+const commentSchema = new Schema({
+   desc: {
+      type: String,
+      required: true,
+   },
+   user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+   },
+   post: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+      required: true,
+   },
+}, { timestamps: true });
+
+export default mongoose.model('Comment', commentSchema);
---
+++ backend/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from 'mongoose';
+
+const postSchema = new Schema({
+   title: {
+      type: String,
+      required: true,
+   },
+   slug: {
+      type: String,
+      required: true,
+      unique: true,
+   },
+   desc: {
+      type: String,
+   },
+   content: {
+      type: String,
+      required: true,
+   },
+   img: {
+      type: String,
+   },
+   isFeatured: {
+      type: Boolean,
+      default: false,
+   },
+   views: {
+      type: Number,
+      default: 0,
+   },
+   user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+   },
+}, { timestamps: true });
+
+export default mongoose.model('Post', postSchema);
