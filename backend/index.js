b23e612987902be15af8d828a2f81c2e1849663e
Aayush <aausgh@gmail.com>
2025-03-31 12:34:19 +0545

    connection made with database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 import express from 'express';
 import 'dotenv/config'
 
+import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.route.js';
+import commentRouter from './routes/comment.route.js';
+import { connectDB } from './lib/connectDb.js';
+
 const app = express();
 
+
+// routes
+app.use("/users", userRouter)
+app.use("/posts", postRouter)
+app.use("/comments", commentRouter)
+
+
+// port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
+   connectDB();
    console.log('Server is running on port ' + port);
 });
